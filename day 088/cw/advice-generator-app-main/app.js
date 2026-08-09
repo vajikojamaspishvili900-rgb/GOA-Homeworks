9329038c014f21e5fc19c999b148cce120d14b6b
vajikojamaspishvili900-rgb <vajikojamaspishvili900@gmail.com>
2026-08-09 17:06:06 +0400

    Frontend Mentor Project: HTML+CSS+JS+API

--- day 088/cw/advice-generator-app-main/app.js
+++ day 088/cw/advice-generator-app-main/app.js
@@ -1,23 +1,32 @@
 // https://api.adviceslip.com/advice
 
 const adviceId = document.querySelector(".advice-id");
-const adviceText = document.querySelector(".advice")
+const adviceText = document.querySelector(".advice");
 const adviceBtn = document.querySelector(".dice-button");
+const advices = [];
 
-
-const getAdvice = async ()=>{
+const getAdvice = async () => {
     const response = await fetch("https://api.adviceslip.com/advice");
     const api = await response.json();
-    // console.log(api)
-    adviceId.innerHTML=`ADVICE #${api.slip.id}`;
-    adviceText.innerHTML=`${api.slip.advice}`;
 
-}
+    adviceId.innerHTML = `ADVICE #${api.slip.id}`;
+    adviceText.innerHTML = api.slip.advice;
 
-getAdvice();
+    return api;
+};
 
-adviceBtn.addEventListener("click", getAdvice);
+adviceBtn.addEventListener("click", async () => {
+    const result = await getAdvice();
 
+    const advice = {
+        id: result.slip.id,
+        advice: result.slip.advice
+    };
+
+    advices.push(advice);
+    console.clear() // clear-s viyeneb radgan axali masivis dabewdvis dros wina masivi agar gamochndes da consoli ar gadaitvirtos
+    console.log(advices);
+});
 
 //  169, advice: 'Do something selfless.'}
 
